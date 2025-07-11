892e324d82517d67facc81fde8e94c379c2b70f4
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-11 06:45:56 +0545

    updated landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Chat from "./components/Chat";
 import FileUpload from "./components/FileUpload";
+
 export default function Home() {
   return (
-    <div>
-      <div className="min-h-screen  w-screen flex">
-        <div className="w-[30%] min-h-screen flex p-4 justify-center items-center">
-          <FileUpload />
-        </div>
-        <div className="w-[70%] border-l-2 ">
-          <Chat />
-        </div>
+    <div className="min-h-screen flex flex-col lg:flex-row">
+      {" "}
+      {/* Responsive layout */}
+      <div className="lg:w-[30%] min-h-[30vh] lg:min-h-screen flex p-4 justify-center items-center border-b-2 lg:border-r-2 lg:border-b-0">
+        <FileUpload />
+      </div>
+      <div className="lg:w-[70%] min-h-[70vh] lg:min-h-screen border-l-0 lg:border-l-2 border-t-2 lg:border-t-0 relative">
+        {" "}
+        {/* Relative for fixed chat input */}
+        <Chat />
       </div>
     </div>
   );
